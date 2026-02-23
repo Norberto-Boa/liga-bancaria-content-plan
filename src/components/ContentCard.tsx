6e5e010838bf9d4f1ff032af7989f8da76c27a6b
Norberto Boa <nboa.26@gmail.com>
2026-02-23 22:06:10 +0200

    feat: adjust card photo to image ratio dynamically

--- src/components/ContentCard.tsx
+++ src/components/ContentCard.tsx
@@ -12,34 +12,37 @@ export function ContentCard({ item, onClick }: Props) {
   return (
     <Card
       onClick={onClick}
-      className="cursor-pointer hover:shadow-xl transition-all hover:scale-[1.02] border-t-4 border-primary"
+      className="cursor-pointer hover:shadow-xl transition-all hover:scale-[1.02] border-none"
     >
-      <div className="p-4 space-y-4">
-        {item.type === "Static" ? (
-          <Badge className="bg-secondary text-black">Cartaz</Badge>
-        ) : (
-          <Badge className="bg-primary">
-            <PlayCircle size={16} className="mr-1" />
-            Reel
-          </Badge>
-        )}
+      <div className="space-y-4">
         {item.type === "Static" && item.image && (
           <img
             src={item.image.src}
             alt={item.title}
-            className="rounded-lg h-58 w-full object-cover"
+            className={`rounded-lg w-full object-cover ${item.image.aspectRatio === "1:1" ? "aspect-square" : "aspect-4/5"}`}
           />
         )}
 
         {item.type === "Dynamic" && (
-          <div className="aspect-square w-full rounded-lg bg-darker/15 flex items-center justify-center">
+          <div className="aspect-4/5 w-full rounded-lg bg-darker/15 flex items-center justify-center">
             <Video size={48} className="text-white opacity-90" />
             {/* <div className="absolute inset-0 bg-black/10 rounded-lg" /> */}
           </div>
         )}
 
-        <div className="flex items-start justify-between flex-col ">
-          <h3 className="font-semibold text-lg text-darker">{item.title}</h3>
+        <div className="px-4 space-y-2 py-3">
+          <div className="flex items-start justify-between flex-col ">
+            <h3 className="font-semibold  text-darker">{item.title}</h3>
+          </div>
+
+          {item.type === "Static" ? (
+            <Badge className="bg-secondary text-black">Cartaz</Badge>
+          ) : (
+            <Badge className="bg-primary">
+              <PlayCircle size={16} className="mr-1" />
+              Reel
+            </Badge>
+          )}
         </div>
       </div>
     </Card>
